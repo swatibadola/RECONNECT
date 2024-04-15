a98f27c2720f9b63bda09ce96666d76997917594
Swati Badola <102805192+swatibadola@users.noreply.github.com>
2024-04-15 10:05:11 +0000

    header

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './Header'
+import Home from './Home'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <Header />
+      <Home />
+    </div>
   )
 }
 
